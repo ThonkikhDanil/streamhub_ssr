f9387df9d9bc9c83ee5915718d775599434e7e27
ThonkikhDanil <daniltonkih575@outlook.com>
2025-05-05 14:45:14 +0500

    Animated sidebar

--- resources/js/Components/Sidebar.vue
+++ resources/js/Components/Sidebar.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import NavLink from '@/Components/NavLink.vue';
-import { inject } from 'vue';
 import { SidebarKey } from '@/keys';
+import { inject } from 'vue';
 
 const sidebar = inject(SidebarKey);
 
@@ -13,22 +13,34 @@ const { isSidebar, toggleSidebar } = sidebar;
 </script>
 
 <template>
-	<div :class="['absolute', 'z-20', 'flex', 'transition-all', isSidebar ? 'visible' : 'hidden']">
-		<div class="bg-[#D5F2F2] dark:bg-[#010326] h-screen flex flex-col">
-			<NavLink :href="route('welcome')" :active="route().current('welcome')" class="mt-0">
-				<div class="mr-3">
-					<i class="fi fi-br-home" />
-				</div>
-				<div>Главная</div>
-			</NavLink>
-			<NavLink :href="route('subscriptions')" :active="route().current('subscriptions')">
-				<div class="mr-3">
-					<i class="fi fi-br-folder" />
-				</div>
-				<div>Подписки</div>
-			</NavLink>
-		</div>
+	<div
+		class="absolute left-[-192px] z-20 flex h-screen flex-col bg-[#D5F2F2] transition-transform duration-500 ease-out dark:bg-[#010326]"
+		:class="{ 'translate-x-48': isSidebar }"
+	>
+		<NavLink
+			:href="route('welcome')"
+			:active="route().current('welcome')"
+			class="mt-0"
+		>
+			<div class="mr-3">
+				<i class="fi fi-br-home" />
+			</div>
+			<div>Главная</div>
+		</NavLink>
+		<NavLink
+			:href="route('subscriptions')"
+			:active="route().current('subscriptions')"
+		>
+			<div class="mr-3">
+				<i class="fi fi-br-folder" />
+			</div>
+			<div>Подписки</div>
+		</NavLink>
 	</div>
 
-	<div @click="toggleSidebar()" :class="['fixed', 'z-10', 'w-screen', 'h-screen', 'transition-all', 'bg-black/25', isSidebar ? 'visible' : 'hidden']"></div>
+	<div
+		@click="toggleSidebar()"
+		class="fixed w-screen h-screen transition-all duration-500 ease-out bg-transparent"
+		:class="{ 'bg-black/35': isSidebar }"
+	></div>
 </template>
