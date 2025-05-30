e57f673fce22bdecebfbf945da38a82779c294cf
ThonkikhDanil <daniltonkih575@outlook.com>
2025-05-30 17:19:55 +0500

    A little more changes

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -5,7 +5,7 @@ import { Link } from '@inertiajs/vue3';
 <template>
 	<Link
 		:href="route('welcome')"
-		class="rounded-2xl border border-transparent text-4xl font-semibold"
+		class="rounded-2xl border border-transparent text-4xl font-semibold transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-[#116EBF] focus-visible:ring-offset-2 dark:focus-visible:ring-[#1F82BF] dark:focus-visible:ring-offset-gray-800"
 	>
 		<span class="text-[#010326] dark:text-[#D5F2F2]">Stream</span>
 		<span class="text-[#116EBF] dark:text-[#1F82BF]">Hub</span>
--- resources/js/Components/LoginButton.vue
+++ resources/js/Components/LoginButton.vue
@@ -4,7 +4,7 @@ import { Link } from '@inertiajs/vue3';
 
 <template>
 	<Link
-		class="flex items-center rounded-2xl px-4 py-1 text-[#010326] transition-all hover:bg-[#116EBF] hover:text-[#D5F2F2] dark:text-[#D5F2F2] dark:hover:bg-[#1F82BF] dark:hover:text-[#010326]"
+		class="flex items-center rounded-2xl border border-transparent px-4 py-1 text-[#010326] transition-all hover:bg-[#116EBF] hover:text-[#D5F2F2] focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-[#116EBF] focus-visible:ring-offset-2 dark:text-[#D5F2F2] dark:hover:bg-[#1F82BF] dark:hover:text-[#010326] dark:focus-visible:ring-[#1F82BF] dark:focus-visible:ring-offset-gray-800"
 		:href="route('login')"
 	>
 		<div class="relative top-1 mr-4 text-2xl">
